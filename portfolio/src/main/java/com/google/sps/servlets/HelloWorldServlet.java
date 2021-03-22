44a2c61e0eab667c2228e7e3f6d66c4a812219e5
dadamu4 <bessiadamu@gmail.com>
2021-03-22 23:07:32 +0000

    Add basic hello wold servlet

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -13,6 +13,6 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Quotes from 'La Casa De Papel'</h1>");
   }
 }
