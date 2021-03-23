9063a23a55fe873b934500810458877a57509281
dadamu4 <bessiadamu@gmail.com>
2021-03-23 20:17:37 +0000

    Add basic fetch functionality

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -14,5 +14,11 @@ public class HelloWorldServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Quotes from 'La Casa De Papel'</h1>");
+    response.getWriter().println("<p>La traición es inherente al amor</p>");
+    response.getWriter().println("<p>Cuanto más fastidiada estás, más felices parecen los días del pasado</p>");
+    response.getWriter().println("<p>Querida, al final el amor es lo que nos hace ver la vida de otro color, y últimamente, sólo lo has visto todo negro</p>");
+    response.getWriter().println("<p>Solo vemos las consecuencias cuando están delante de nuestras narices</p>");
+    response.getWriter().println("<p>Te han enseñado en la vida a diferenciar de buenos y malos. Si esto lo hacen otras personas, no pasaría nada</p>");
   }
+  
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,10 +36,10 @@ function addRandomAvatarQuote() {
 }
 
 // fetch function
-// async function showServerTime() {
-//   const responseFromServer = await fetch('/date');
-//   const textFromResponse = await responseFromServer.text();
+async function showServerTime() {
+  const responseFromServer = await fetch('/date');
+  const textFromResponse = await responseFromServer.text();
 
-//   const dateContainer = document.getElementById('date-container');
-//   dateContainer.innerText = textFromResponse;
-// } 
+  const dateContainer = document.getElementById('date-container');
+  dateContainer.innerText = textFromResponse;
+} 
